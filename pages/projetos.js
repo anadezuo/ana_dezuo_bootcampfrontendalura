e062b8774908070f95909eacdfd8a30c47a5ccd5
Ana Dezuó <ana.dezuo.apd@gmail.com>
2021-08-28 23:24:14 -0300

    feat(seo): ajustando a rotina de seo
    Ajustando a rotina de SEO para ficar reutilizavél para outras páginas, e adicionado o arquivo de
    robots.txt

--- pages/projetos.js
+++ pages/projetos.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import SEO from '../src/core/Head/SEO';
 import Cabecalho from '../src/components/Cabecalho';
 import Projetos from '../src/components/Projetos';
 import Box from '../src/components/foundation/layout/Box';
 
 export default function ProjetosPage({
-  theme, setTheme, isThemeDark, setIsThemeDark,
+  theme,
+  setTheme,
+  isThemeDark,
+  setIsThemeDark,
 }) {
   return (
-    <Box
-      marginTop={{ xs: '60px', md: '80px' }}
-    >
-      <Cabecalho
-        setTheme={setTheme}
-        isThemeDark={isThemeDark}
-        setIsThemeDark={setIsThemeDark}
-      />
-      <Projetos theme={theme} />
-    </Box>
+    <>
+      <SEO headTitle="Projetos" />
+      <Box marginTop={{ xs: '60px', md: '80px' }}>
+        <Cabecalho
+          setTheme={setTheme}
+          isThemeDark={isThemeDark}
+          setIsThemeDark={setIsThemeDark}
+        />
+        <Projetos theme={theme} />
+      </Box>
+    </>
   );
 }
 
--- pages/sobre.js
+++ pages/sobre.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import SEO from '../src/core/Head/SEO';
 import Cabecalho from '../src/components/Cabecalho';
 import Box from '../src/components/foundation/layout/Box';
 
@@ -7,13 +8,16 @@ export default function Sobre({ setTheme, isThemeDark, setIsThemeDark }) {
   // const [modal, setModal] = useState(false);
 
   return (
-    <Box width="100vw" height="100vh" display="flex">
-      <Cabecalho
-        setTheme={setTheme}
-        isThemeDark={isThemeDark}
-        setIsThemeDark={setIsThemeDark}
-      />
-    </Box>
+    <>
+      <SEO headTitle="Sobre Ana" />
+      <Box width="100vw" height="100vh" display="flex">
+        <Cabecalho
+          setTheme={setTheme}
+          isThemeDark={isThemeDark}
+          setIsThemeDark={setIsThemeDark}
+        />
+      </Box>
+    </>
   );
 }
 
